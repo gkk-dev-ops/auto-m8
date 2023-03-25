8b0d6ea4e5b490cd7348a261d0f3eb266abc8bdc
gkk <gkk.dev.ops@gmail.com>
2023-03-25 14:38:04 +0100

    Add tab recogniction

--- TODO.md
+++ TODO.md
@@ -1,10 +1,17 @@
 # TODOs
 
-(A) Support savings selectors to any site
-(B) Support saving actions - code possible to inject and run on chose site
+(A) Support savings selectors: `site: [selectors..]` in local storage - (inform of saving by displaying spinner, since there is no service worker - site dies after closing popu window).
+
+(B) Support saving actions - save code snippets as action possible to run on current tab.
 e.g.:
-Save snippet below to open all options on notification view on linkedin.com
+Save code snippets below to open all options on notification view on linkedin.com
 
 ```js
 $$('li-icon.nt-card-settings-dropdown__trigger-icon').forEach(el => el.click())
 ```
+
+User should be able to:
+
+- see the action name
+- lookup action code
+- run the action by clicking single button
---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+  function openAllLinkedinNotifications() {
+  const LINKEDIN_NOTIFICATIONS_OPTIONS_DROPDOWN_SELECTOR = 'li-icon.nt-card-settings-dropdown__trigger-icon'
+  document.querySelectorAll(LINKEDIN_NOTIFICATIONS_OPTIONS_DROPDOWN_SELECTOR).forEach((el) => {
+    if (el instanceof HTMLElement) {
+      el.click();
+}
+  })
+}
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
